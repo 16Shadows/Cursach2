7328441f0c16dcf6df2b8f2c4befc9ec5e217a47
16Shadows <egor.0.00@mail.ru>
2023-03-13 22:47:25 +0500

    Implemented WeakDelegate and WeakEvent to provide weak event pattern.

---
+++ CSToolbox/WeakDelegate.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Reflection;
+
+namespace CSToolbox
+{
+    /// <summary>
+    /// A delegate which does not hold a reference to the object it's invoked on
+    /// Sadly, much slower than <see cref="Delegate"/> because it uses reflection
+    /// </summary>
+    public class WeakDelegate
+    {
+        private MethodInfo Method { get; }
+        private WeakReference Object { get; }
+
+        /// <summary>
+        /// Returns true if the target object is still alive and the delegate can be called
+        /// </summary>
+        public bool IsAlive => Object.IsAlive;
+
+        public WeakDelegate(MethodInfo method, object target)
+        {
+            Method = method ?? throw new ArgumentNullException(nameof(method));
+            if (Method.IsStatic)
+                throw new ArgumentException("Static methods are not supported by WeakDelegate", nameof(method));
+            else if (target == null)
+                throw new ArgumentNullException(nameof(target));
+            Object = new WeakReference(target);
+        }
+
+        public WeakDelegate(Delegate del) : this(del?.Method, del?.Target) {}
+
+        /// <summary>
+        /// Invokes this delegate.
+        /// </summary>
+        /// <param name="args">The arguments for the delegate call (similar to args of DynamicInvoke() on Delegate).</param>
+        /// <returns>Delegate call result. It a call is impossible, returns null.</returns>
+        public object? Invoke(object?[]? args)
+        {
+            if (!IsAlive)
+                return null;
+            return Method.Invoke(Object.Target, args);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is WeakDelegate other && other.Object.Target == Object.Target && other.Method.Equals(Method);
+        }
+
+        public override int GetHashCode() => base.GetHashCode();
+    }
+
+    public sealed class WeakDelegate<RetType> : WeakDelegate
+    {
+        public delegate RetType CallType();
+
+        public WeakDelegate(CallType del) : base(del) {}
+
+        public RetType Invoke() => (RetType)Invoke(null);
+    }
+
+    public sealed class WeakDelegate<RetType, Arg1> : WeakDelegate
+    {
+        public delegate RetType CallType(Arg1 arg1);
+
+        public WeakDelegate(CallType del) : base(del) {}
+
+        public RetType Invoke(Arg1 arg1) => (RetType)Invoke(new object[] {arg1});
+    }
+
+    public sealed class WeakDelegate<RetType, Arg1, Arg2> : WeakDelegate
+    {
+        public delegate RetType CallType(Arg1 arg1, Arg2 arg2);
+
+        public WeakDelegate(CallType del) : base(del) {}
+
+        public RetType Invoke(Arg1 arg1, Arg2 arg2) => (RetType)Invoke(new object[] {arg1, arg2});
+    }
+
+    public sealed class WeakDelegate<RetType, Arg1, Arg2, Arg3> : WeakDelegate
+    {
+        public delegate RetType CallType(Arg1 arg1, Arg2 arg2, Arg3 arg3);
+
+        public WeakDelegate(CallType del) : base(del) {}
+
+        public RetType Invoke(Arg1 arg1, Arg2 arg2, Arg3 arg3) => (RetType)Invoke(new object[] {arg1, arg2, arg3});
+    }
+
+    public sealed class WeakDelegate<RetType, Arg1, Arg2, Arg3, Arg4> : WeakDelegate
+    {
+        public delegate RetType CallType(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4);
+
+        public WeakDelegate(CallType del) : base(del) {}
+
+        public RetType Invoke(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4) => (RetType)Invoke(new object[] {arg1, arg2, arg3, arg4});
+    }
+
+    public sealed class WeakDelegate<RetType, Arg1, Arg2, Arg3, Arg4, Arg5> : WeakDelegate
+    {
+        public delegate RetType CallType(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5);
+
+        public WeakDelegate(CallType del) : base(del) {}
+
+        public RetType Invoke(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5) => (RetType)Invoke(new object[] {arg1, arg2, arg3, arg4, arg5});
+    }
+
+    public sealed class WeakDelegate<RetType, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6> : WeakDelegate
+    {
+        public delegate RetType CallType(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6);
+
+        public WeakDelegate(CallType del) : base(del) {}
+
+        public RetType Invoke(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6) => (RetType)Invoke(new object[] {arg1, arg2, arg3, arg4, arg5, arg6});
+    }
+
+    public sealed class WeakDelegate<RetType, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7> : WeakDelegate
+    {
+        public delegate RetType CallType(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7);
+
+        public WeakDelegate(CallType del) : base(del) {}
+
+        public RetType Invoke(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7) => (RetType)Invoke(new object[] {arg1, arg2, arg3, arg4, arg5, arg6, arg7});
+    }
+
+    public sealed class WeakDelegate<RetType, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8> : WeakDelegate
+    {
+        public delegate RetType CallType(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8);
+
+        public WeakDelegate(CallType del) : base(del) {}
+
+        public RetType Invoke(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8) => (RetType)Invoke(new object[] {arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8});
+    }
+
+    public sealed class WeakAction : WeakDelegate
+    {
+        public delegate void CallType();
+
+        public WeakAction(CallType del) : base(del) {}
+
+        public void Invoke() => Invoke(null);
+    }
+
+    public sealed class WeakAction<Arg1> : WeakDelegate
+    {
+        public delegate void CallType(Arg1 arg1);
+
+        public WeakAction(CallType del) : base(del) {}
+
+        public void Invoke(Arg1 arg1) => Invoke(new object[] {arg1});
+    }
+
+    public sealed class WeakAction<Arg1, Arg2> : WeakDelegate
+    {
+        public delegate void CallType(Arg1 arg1, Arg2 arg2);
+
+        public WeakAction(CallType del) : base(del) {}
+
+        public void Invoke(Arg1 arg1, Arg2 arg2) => Invoke(new object[] {arg1, arg2});
+    }
+
+    public sealed class WeakAction<Arg1, Arg2, Arg3> : WeakDelegate
+    {
+        public delegate void CallType(Arg1 arg1, Arg2 arg2, Arg3 arg3);
+
+        public WeakAction(CallType del) : base(del) {}
+
+        public void Invoke(Arg1 arg1, Arg2 arg2, Arg3 arg3) => Invoke(new object[] {arg1, arg2, arg3});
+    }
+
+    public sealed class WeakAction<Arg1, Arg2, Arg3, Arg4> : WeakDelegate
+    {
+        public delegate void CallType(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4);
+
+        public WeakAction(CallType del) : base(del) {}
+
+        public void Invoke(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4) => Invoke(new object[] {arg1, arg2, arg3, arg4});
+    }
+
+    public sealed class WeakAction<Arg1, Arg2, Arg3, Arg4, Arg5> : WeakDelegate
+    {
+        public delegate void CallType(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5);
+
+        public WeakAction(CallType del) : base(del) {}
+
+        public void Invoke(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5) => Invoke(new object[] {arg1, arg2, arg3, arg4, arg5});
+    }
+
+    public sealed class WeakAction<Arg1, Arg2, Arg3, Arg4, Arg5, Arg6> : WeakDelegate
+    {
+        public delegate void CallType(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6);
+
+        public WeakAction(CallType del) : base(del) {}
+
+        public void Invoke(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6) => Invoke(new object[] {arg1, arg2, arg3, arg4, arg5, arg6});
+    }
+
+    public sealed class WeakAction<Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7> : WeakDelegate
+    {
+        public delegate void CallType(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7);
+
+        public WeakAction(CallType del) : base(del) {}
+
+        public void Invoke(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7) => Invoke(new object[] {arg1, arg2, arg3, arg4, arg5, arg6, arg7});
+    }
+
+    public sealed class WeakAction<Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8> : WeakDelegate
+    {
+        public delegate void CallType(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8);
+
+        public WeakAction(CallType del) : base(del) {}
+
+        public void Invoke(Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4, Arg5 arg5, Arg6 arg6, Arg7 arg7, Arg8 arg8) => Invoke(new object[] {arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8});
+    }
+}
---
+++ CSToolbox/WeakEvent.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSToolbox
+{
+    /// <summary>
+    /// A weak event implementation
+    /// </summary>
+    public class WeakEvent
+    {
+        protected List<WeakDelegate> DelegateList { get; }
+
+        public WeakEvent()
+        {
+            DelegateList = new List<WeakDelegate>();
+        }
+
+        /// <summary>
+        /// Invokes the event with provided parameters
+        /// </summary>
+        /// <param name="parameters">The parameters. Same as parameters of <see cref="Delegate.DynamicInvoke(object?[]?)"/>.</param>
+        public void Invoke(object?[]? parameters)
+        {
+            WeakDelegate target;
+            for (int i = 0; i < DelegateList.Count;)
+            {
+                target = DelegateList[i];
+                if (target.IsAlive)
+                {
+                    target.Invoke(parameters);
+                    i++;
+                }
+                else
+                {
+                    DelegateList[i] = DelegateList[DelegateList.Count - 1];
+                    DelegateList.RemoveAt(DelegateList.Count - 1);
+                }
+            }
+        }
+
+        public WeakEvent Subscribe(WeakDelegate target)
+        {
+            if (target == null)
+                throw new ArgumentNullException(nameof(target));
+
+            DelegateList.Add(target);
+            return this;
+        }
+        public WeakEvent Subscribe(Delegate target) => Subscribe(new WeakDelegate(target));
+
+        public WeakEvent Unsubscribe(WeakDelegate target)
+        {
+            if (target == null)
+                throw new ArgumentNullException(nameof(target));
+
+            int id = DelegateList.IndexOf(target);
+            if (id != -1)
+            {
+                DelegateList[id] = DelegateList[DelegateList.Count - 1];
+                DelegateList.RemoveAt(DelegateList.Count - 1);
+            }
+            return this;
+        }
+        public WeakEvent Unsubscribe(Delegate target) => Unsubscribe(new WeakDelegate(target));
+
+        public static WeakEvent operator+(WeakEvent weakEvent, WeakDelegate target) => weakEvent.Subscribe(target);
+
+        public static WeakEvent operator-(WeakEvent weakEvent, WeakDelegate target) => weakEvent.Unsubscribe(target);
+    }
+
+    public sealed class WeakEvent<Arg1>
+    {
+        public delegate void CallType(Arg1 arg1);
+
+        private WeakEvent Event { get; } = new();
+
+        public void Invoke(Arg1 arg1) => Event.Invoke(new object[] {arg1});
+
+        public WeakEvent<Arg1> Subscribe(WeakAction<Arg1> target)
+        {
+            Event.Subscribe(target);
+            return this;
+        }
+
+        public WeakEvent<Arg1> Unsubscribe(WeakAction<Arg1> target)
+        {
+            Event.Unsubscribe(target);
+            return this;
+        }
+
+        public WeakEvent<Arg1> Subscribe(CallType target)
+        {
+            Event.Subscribe(target);
+            return this;
+        }
+
+        public WeakEvent<Arg1> Unsubscribe(CallType target)
+        {
+            Event.Unsubscribe(target);
+            return this;
+        }
+
+        public static WeakEvent<Arg1> operator+(WeakEvent<Arg1> weakEvent, WeakAction<Arg1> target) => weakEvent.Subscribe(target);
+        public static WeakEvent<Arg1> operator-(WeakEvent<Arg1> weakEvent, WeakAction<Arg1> target) => weakEvent.Unsubscribe(target);
+    }
+
+    public sealed class WeakEvent<Arg1, Arg2>
+    {
+        public delegate void CallType(Arg1 arg1, Arg2 arg2);
+
+        private WeakEvent Event { get; } = new();
+
+        public void Invoke(Arg1 arg1, Arg2 arg2) => Event.Invoke(new object[] {arg1, arg2});
+
+        public WeakEvent<Arg1, Arg2> Subscribe(WeakAction<Arg1, Arg2> target)
+        {
+            Event.Subscribe(target);
+            return this;
+        }
+
+        public WeakEvent<Arg1, Arg2> Unsubscribe(WeakAction<Arg1, Arg2> target)
+        {
+            Event.Unsubscribe(target);
+            return this;
+        }
+
+        public WeakEvent<Arg1, Arg2> Subscribe(CallType target)
+        {
+            Event.Subscribe(target);
+            return this;
+        }
+
+        public WeakEvent<Arg1, Arg2> Unsubscribe(CallType target)
+        {
+            Event.Unsubscribe(target);
+            return this;
+        }
+
+        public static WeakEvent<Arg1, Arg2> operator+(WeakEvent<Arg1, Arg2> weakEvent, WeakAction<Arg1, Arg2> target) => weakEvent.Subscribe(target);
+        public static WeakEvent<Arg1, Arg2> operator-(WeakEvent<Arg1, Arg2> weakEvent, WeakAction<Arg1, Arg2> target) => weakEvent.Unsubscribe(target);
+    }
+}
