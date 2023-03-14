cdc83df715c369309979a5cbbc26e4e717edc864
16Shadows <egor.0.00@mail.ru>
2023-03-14 22:17:59 +0500

    Added ReadOnlyLazyProperty, added INotifyPropertyChanged for lazy property's Value. Tweaked WeakEvent.Subscribe's argument.

--- CSToolbox/LazyProperty.cs
+++ CSToolbox/LazyProperty.cs
@@ -1,28 +1,49 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace CSToolbox
 {
     /// <summary>
-    /// Represents a lazy property which doesn't have a value until its fetched or set for the first time
+    /// Represents a lazy property which doesn't have a value until it's fetched or set for the first time
     /// </summary>
     /// <typeparam name="T">The type of this property</typeparam>
-    public class LazyProperty<T>
+    public class LazyProperty<T> : INotifyPropertyChanged
     {
+        /// <summary>
+        /// The method to use for initialization. May never be called if the value is set before first fetch.
+        /// </summary>
+        /// <param name="property">A reference to the property which is being initialized</param>
         public delegate void LoaderType(LazyProperty<T> property);
-        public WeakEvent<LazyProperty<T>> PropertyChanged { get; } = new();
+
+        /// <summary>
+        /// Raised when this property changes. 
+        /// Implemented using <see cref="WeakEvent"/> to allow the object holding this property to subscribe to it without causing a memory leak.
+        /// </summary>
+        public WeakEvent<LazyProperty<T>> WeakPropertyChanged { get; } = new();
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         private LoaderType m_Loader;
         private T? m_Value;
         private bool m_Loaded;
+
+        /// <summary>
+        /// The value this property has
+        /// </summary>
         public T? Value
         {
             get
             {
                 if (!m_Loaded)
                 {
-                    m_Loader(this);
-                    m_Loader = null;
-                    m_Loaded = true;
+                    lock (m_Value)
+                    {
+                        if (!m_Loaded)
+                        {
+                            m_Loader(this);
+                            m_Loader = null;
+                            m_Loaded = true;
+                        }
+                    }
                 }
                 return m_Value;
             }
@@ -30,13 +51,104 @@ namespace CSToolbox
             {
                 if (m_Value?.Equals(value) == true)
                     return;
-                m_Value = value;
-                m_Loaded = true;
-                PropertyChanged.Invoke(this);
+
+                if (!m_Loaded)
+                {
+                    lock (m_Value)
+                    {
+                        if (!m_Loaded)
+                        {
+                            m_Value = value;
+                            m_Loaded = true;
+                        }
+                    }
+                }
+                
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+                WeakPropertyChanged.Invoke(this);
             }
         }
 
+        /// <summary>
+        /// Create a lazy property
+        /// </summary>
+        /// <param name="loader">Initialization method</param>
+        /// <exception cref="ArgumentNullException">Can be thrown if loader is null</exception>
         public LazyProperty(LoaderType loader)
+        {
+            m_Loader = loader ?? throw new ArgumentNullException(nameof(loader));
+        }
+    }
+
+    /// <summary>
+    /// Represents a lazy property which doesn't have a value until it's fetched or set for the first time. After being initializer, the value cannot be set.
+    /// </summary>
+    /// <typeparam name="T">The type of this property</typeparam>
+    public class ReadOnlyLazyProperty<T> : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The method to use for initialization. May never be called if the value is set before first fetch.
+        /// </summary>
+        /// <param name="property">A reference to the property which is being initialized</param>
+        public delegate void LoaderType(ReadOnlyLazyProperty<T> property);
+
+        /// <summary>
+        /// Raised when this property changes. 
+        /// Implemented using <see cref="WeakEvent"/> to allow the object holding this property to subscribe to it without causing a memory leak.
+        /// </summary>
+        public WeakEvent<ReadOnlyLazyProperty<T>> WeakPropertyChanged { get; } = new();
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private LoaderType m_Loader;
+        private T? m_Value;
+        private bool m_Loaded;
+
+        /// <summary>
+        /// The value this property has
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Can be raised when attempting to set the value of an already initializer property</exception>
+        public T? Value
+        {
+            get
+            {
+                if (!m_Loaded)
+                {
+                    lock (m_Value)
+                    {
+                        if (!m_Loaded)
+                        {
+                            m_Loader(this);
+                            m_Loader = null;
+                            m_Loaded = true;
+                        }
+                    }
+                }
+                return m_Value;
+            }
+            set
+            {
+                if (m_Loaded)
+                    throw new InvalidOperationException();
+                lock (m_Value)
+                {
+                    if (!m_Loaded)
+                    {
+                        m_Value = value;
+                        m_Loaded = true;
+                    }
+                }
+                
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+                WeakPropertyChanged.Invoke(this);
+            }
+        }
+
+        /// <summary>
+        /// Create a lazy property
+        /// </summary>
+        /// <param name="loader">Initialization method</param>
+        /// <exception cref="ArgumentNullException">Can be thrown if loader is null</exception>
+        public ReadOnlyLazyProperty(LoaderType loader)
         {
             m_Loader = loader ?? throw new ArgumentNullException(nameof(loader));
         }
