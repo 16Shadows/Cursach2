6e38a339c3b42cea62f4fb99366db9f528976c6d
16Shadows <egor.0.00@mail.ru>
2023-03-14 22:58:45 +0500

    Started implementing ViewModels.

---
+++ DMOrganizerViewModel/DocumentViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CSToolbox;
+using DMOrganizerModel.Interface.Items;
+using MVVMToolbox;
+using System;
+using System.Collections.ObjectModel;
+
+namespace DMOrganizerViewModel
+{
+    public class DocumentViewModel : SectionViewModel
+    {
+        public ReadOnlyLazyProperty<ObservableCollection<string>> Tags { get; }
+
+        protected IDocument Document { get; }
+
+        public DocumentViewModel(IContext context, IServiceProvider serviceProvider, IDocument document) : base(context, serviceProvider, document)
+        {
+            Document = document;
+
+            Tags = new ReadOnlyLazyProperty<ObservableCollection<string>>(p =>
+            {
+                WeakAction<IDocument, DocumentCurrentTagsEventArgs>.CallType handler = (_, e) => Context.Invoke(() => p.Value = new ObservableCollection<string>(e.Tags));
+                Document.DocumentCurrentTags.Subscribe(handler);
+                Document.RequestDocumentCurrentTags();
+            });
+            Document.DocumentTagsChanged.Subscribe(Document_TagsChanged);
+        }
+
+        private void Document_TagsChanged(IDocument sender, DocumentTagsChangedEventArgs e)
+        {
+            if (!e.HasChanged)
+                return;
+            else if (e.Type == DocumentTagsChangedEventArgs.ChangeType.TagAdded)
+                Context.Invoke(() => Tags.Value.Add(e.Tag));
+            else
+                Context.Invoke(() => Tags.Value.Remove(e.Tag));
+        }
+    }
+}
---
+++ DMOrganizerViewModel/NamedItemViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CSToolbox;
+using DMOrganizerModel.Interface.Items;
+using MVVMToolbox;
+using System;
+
+namespace DMOrganizerViewModel
+{
+    public class NamedItemViewModel : ViewModelBase
+    {
+        public LazyProperty<string?> Name;
+
+        protected INamedItem Item { get; }
+
+        public NamedItemViewModel(IContext context, IServiceProvider serviceProvider, INamedItem section) : base(context, serviceProvider)
+        {
+            Item = section;
+
+            Name = new LazyProperty<string?>(p =>
+            {
+                WeakAction<INamedItem, NamedItemNameChangedEventArgs>.CallType handler = (_, e) => Context.Invoke(() => p.Value = e.Name);
+                Item.ItemNameChanged.Subscribe( handler );
+                Item.RequestItemNameUpdate();
+            });
+
+            Item.ItemNameChanged.Subscribe(NamedItem_ItemNameChanged);
+        }
+
+        private void NamedItem_ItemNameChanged(INamedItem sender, NamedItemNameChangedEventArgs e)
+        {
+            if (!e.HasChanged)
+                return;
+            Context.Invoke(() => Name.Value = e.Name);
+        }
+    }
+}
