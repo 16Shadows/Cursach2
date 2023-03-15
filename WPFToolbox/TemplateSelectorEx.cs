cf97f69a798e2d5cc46210fffb3b885859ff43f0
16Shadows <egor.0.00@mail.ru>
2023-03-15 23:57:28 +0500

    Added a general-purpose template selector which selects a template if the item is of its datatype or is inherited from it. Added a null to localized "Loading..." string converted.

---
+++ WPFToolbox/TemplateSelectorEx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFToolbox
+{
+    public class TemplateSelectorEx : DataTemplateSelector
+    {
+        public List<DataTemplate> Templates { get; } = new();
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (Templates == null)
+                return base.SelectTemplate(item, container);
+
+            foreach (DataTemplate template in Templates)
+            {
+                if (template.DataType is not Type type)
+                    continue;
+                else if (!item.GetType().IsSubclassOf(type) && item.GetType() != type)
+                    continue;
+                return template;
+            }
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
