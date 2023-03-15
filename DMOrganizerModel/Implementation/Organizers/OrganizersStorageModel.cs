6b389b533d64c91b58b685130a4b2861868a6ee0
16Shadows <egor.0.00@mail.ru>
2023-03-15 19:08:58 +0500

    Updated StorageModel to allow organizer loading.

---
+++ DMOrganizerModel/Implementation/Organizers/OrganizersStorageModel.cs
@@ -0,0 +1,17 @@
+﻿using CSToolbox.Weak;
+using DMOrganizerModel.Interface.Organizer;
+
+namespace DMOrganizerModel.Implementation.Organizers
+{
+    public static class OrganizersStorageModel
+    {
+        private static WeakCache<string, Organizer> OrganizersCache { get; } = new(path => new Organizer(path));
+
+        /// <summary>
+        /// Loads .dmo file located at specified path
+        /// </summary>
+        /// <param name="path">The path to file</param>
+        /// <returns>The Organizer stored in the specified file</returns>
+        public static IOrganizer LoadOrganizer(string path) => OrganizersCache[path];
+    }
+}
