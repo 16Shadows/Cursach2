eae4950d4658461527f2d162fbc1bb6bfe796608
16Shadows <egor.0.00@mail.ru>
2023-03-17 16:38:59 +0500

    Added dummy services. Added cotext menus to items in navigation tree.

---
+++ DMOrganizerApp/Services/DMOrganizerAppInputBoxService.cs
@@ -0,0 +1,19 @@
+﻿using DMOrganizerViewModel;
+using MVVMToolbox;
+using MVVMToolbox.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMOrganizerApp.Services
+{
+    internal class DMOrganizerAppInputBoxService : IInputBoxService<OrganizerInputBoxScenarios>
+    {
+        public InputBoxResult Show<ReturnType>(InputBoxConfiguration<OrganizerInputBoxScenarios, ReturnType> configuration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DMOrganizerApp/Services/DMOrganizerAppServiceProvider.cs
+++ DMOrganizerApp/Services/DMOrganizerAppServiceProvider.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using CSToolbox;
+using DMOrganizerViewModel;
+using MVVMToolbox.Services;
+using System;
 
 namespace DMOrganizerApp.Services
 {
-    internal class DMOrganizerAppServiceProvider : IServiceProvider
+    internal sealed class DMOrganizerAppServiceProvider : IServiceProvider
     {
+        private DMOrganizerAppInputBoxService InputBoxService { get; }
+        private DMOrganizerAppNotificationService NotificationService { get; }
+
+        public DMOrganizerAppServiceProvider()
+        {
+            InputBoxService = new DMOrganizerAppInputBoxService();
+            NotificationService = new DMOrganizerAppNotificationService();
+        }
+
         public object? GetService(Type serviceType)
         {
-            //Dummy for now
+            if (serviceType.Is(typeof(INotificationService<OrganizerNotificationScenarios>)))
+                return NotificationService;
+            else if (serviceType.Is(typeof(IInputBoxService<OrganizerInputBoxScenarios>)))
+                return InputBoxService;
             return null;
         }
     }
